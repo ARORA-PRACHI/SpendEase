1bec7afd7068a3e0130d885b5d3c6062e1a62755
My Name <prisha682.be22@chitkara.edu.in>
2024-11-22 14:26:16 +0530

    chat_backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDb = require('../server/db/dbConnection');
 const userRouter = require('./routers/userRouter');
+const chatRouter = require('./routers/chatRouter'); 
 const app = express();
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 connectDb();
 
 app.use('/users',userRouter);
+app.use('/chat', chatRouter);
 const port =  process.env.PORT_NO || 4000 ;
 app.listen(port , ()=>{
         console.log(`Server on :- ${port}`);
