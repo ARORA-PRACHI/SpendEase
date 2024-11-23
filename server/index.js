ac743d98ff211029f3da8660d4752a15ee464147
Prachi <prachi659.be22@chitkara.edu.in>
2024-11-23 12:22:33 +0530

    Expense Backend Added

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDb = require('../server/db/dbConnection');
 
+const expenseRouter = require('./routers/expenseRouter')
 const app = express();
 dotenv.config();
 
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/expenses',expenseRouter);
 connectDb();
 
 const port =  process.env.PORT_NO || 4000 ;
